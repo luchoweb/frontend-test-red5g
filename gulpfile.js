9443fd0251af43167473f9ff84a3227261ea9abb
Lucho Web <luchoweb.dev@gmail.com>
2024-03-08 09:52:02 -0500

    Initial files and gulp installed to compile sass

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require("gulp");
+const sass = require('gulp-sass')(require('sass'));
+
+function compileSass() {
+  return gulp
+    .src("assets/scss/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("assets/css"));
+}
+gulp.task("sass", compileSass);
+
+function watchSass() {
+  gulp.watch("assets/scss/*.scss", compileSass);
+}
+gulp.task("watch", watchSass);
